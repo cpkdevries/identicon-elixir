50132be0034c4e431d0557173085a28ea82993e1
Cody deVries <=>
2016-11-10 19:31:54 -0500

    Hash an input string.

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,2 +1,13 @@
 defmodule Identicon do
+    def main(input) do
+        # take input and pass it into hash_input as the first arg.
+        input
+        |> hash_input
+    end
+
+    def hash_input(input) do
+        # first hash, then pass to :binary.bin_to_list
+        :crypto.hash(:md5, input)
+        |> :binary.bin_to_list
+    end
 end
