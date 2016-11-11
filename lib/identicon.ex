ff62a2c53071d3394a08effc21f57491bb56f228
Cody deVries <=>
2016-11-11 11:13:48 -0500

    Image struct to hold hex values and color rgb values

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,13 +1,20 @@
 defmodule Identicon do
-    def main(input) do
-        # take input and pass it into hash_input as the first arg.
-        input
-        |> hash_input
-    end
+  def main(input) do
+    # take input and pass it into hash_input as the first arg.
+    input
+    |> hash_input
+    |> pick_color
+  end
 
-    def hash_input(input) do
-        # first hash, then pass to :binary.bin_to_list
-        :crypto.hash(:md5, input)
-        |> :binary.bin_to_list
-    end
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}} # update Image struct.
+  end
+
+  def hash_input(input) do
+    # first hash, then pass to :binary.bin_to_list
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
 end
